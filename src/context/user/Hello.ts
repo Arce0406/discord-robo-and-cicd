af50836989622bb9f51d2cd853c856fcc1acb97d
FeatherArce <ice810406@gmail.com>
2024-06-15 03:13:40 +0800

    Add sample: context/user/Hello.ts for context menu sample

---
+++ src/context/user/Hello.ts
@@ -0,0 +1,11 @@
+import type { CommandConfig } from 'robo.js'
+import type { UserContextMenuCommandInteraction, GuildMember } from 'discord.js'
+
+export const config: CommandConfig = {
+    description: 'Hello world!',
+
+}
+
+export default async function (interaction: UserContextMenuCommandInteraction, user: GuildMember) {
+    interaction.reply(`Hello, ${user.displayName}!`)
+}
