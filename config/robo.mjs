00294ea8d284fffaa68f124b53c6eeb466fbbe05
FeatherArce <ice810406@gmail.com>
2024-06-15 03:14:29 +0800

    Update settings

--- config/robo.mjs
+++ config/robo.mjs
@@ -5,11 +5,19 @@
  **/
 export default {
 	clientOptions: {
-		intents: ['Guilds', 'GuildMessages']
+		intents: [
+			'Guilds', 'GuildMessages', 'GuildMembers', 'GuildMessageReactions', 'GuildEmojisAndStickers', 'GuildPresences', 'GuildIntegrations', 'GuildWebhooks', 'GuildInvites', 'GuildVoiceStates', 'GuildMessageTyping',
+			'DirectMessages', 'DirectMessageReactions', 'DirectMessageTyping',
+			'MessageContent',
+		]
 	},
 	plugins: [],
 	type: 'robo',
+	logger: {
+		level: 'debug',
+	},
 	experimental: {
-		userInstall: true
+		userInstall: true,
+		incrementalBuilds: true,
 	}
 }
